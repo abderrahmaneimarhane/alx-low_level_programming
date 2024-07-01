9c88dc3103bd9606d88e94c98c943ae2d4d7cb95
abderrahmaneimarhane <abderrahmane.imarhane.001@gmail.com>
2024-07-01 16:02:36 -0700

    Add README.md file with initial content

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
-/**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  Owned By Bwave/Bright
-*  Return: 0 is success
-*/
 
+/**
+ * reset_to_98 - updates the value of the integer to 98
+ * @n: pointer to the integer we want to set to 98
+ */
 void reset_to_98(int *n)
 {
-	*n = 98;
+    *n = 98;
 }
