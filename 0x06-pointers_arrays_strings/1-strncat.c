85cbe1bed6dc6b026c16bc6a7d7be2a45e0b0b42
abderrahmaneimarhane <abderrahmane.imarhane.001@gmail.com>
2024-07-04 13:28:23 -0700

    Add README.md file with initial content

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * _strncat - function concatenates two strings
+ * _strncat - concatenates two strings
  *
- * %dest: poiter to destination input 
- * %src: pointer to source input
- * %n: most number of bytes from src
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
  *
- * Return:%dest
+ * Return: pointer to the resulting string dest
  */
-
-char *_strncat(char * dest, charr *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int c, i;
+	int i, j;
 
-	c = 0;
-
-	/*find size of dest arrays*/
-	while (dest[c])
-		c++;
-
-	/**
-	 * src does not need to be null terminated
-	 * if it contains nor more bytes
-	 */
-	for (i =0 ; i < n && src[i] != '\0'; i++)
-		dest[c + i] = src[i];
-	/* null terminate dest*/
-	dest[c + i] = '\0';
-
-	return  (dest);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
 }
