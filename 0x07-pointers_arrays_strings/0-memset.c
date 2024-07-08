003ef348ddf8c9a56a8c37f05ed32332713b73ed
abderrahmaneimarhane <abderrahmane.imarhane.001@gmail.com>
2024-07-08 14:57:14 -0700

    Add README.md file with initial content

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * _memset - function fill the first @n bytes of the
- * memory area pointed
+ * _memset - function fill the first @n bytes of the memory area pointed
  * to by @s with the constant byte @b
  *
  * @n: bytes of the memory area pointed to by @s
  *
- * @s: with the constant byte @b
+ * @z: with the constant byte @b
  *
  * @b: memory area pointer
  *
  * Return: a pointer to the memory area @s
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *z, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
 	while (i < n)
 	{
-	s[i] = b;
+	z[i] = b;
 	i++;
 	}
 
-	return (s);
+	return (z);
 }
