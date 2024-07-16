ddd64d171174d87db1a45a1e13b581336300e0b5
abderrahmaneimarhane <abderrahmane.imarhane.001@gmail.com>
2024-07-16 10:27:59 -0700

    Add README.md file with initial content

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,30 +1,24 @@
 #include "main.h"
-
+#include <stdlib.h>
 
 /**
- * create_array - creates an array of chars,
- * and initializes it with a specific char.
- * @size: size of the array.
- * @c: char to initialize the array with.
- * Return: a pointer to the array, or NULL if it fails.
+ * create_array - create array of size size and assign char c
+ * @size: size of array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  */
-
-
 char *create_array(unsigned int size, char c)
 {
-	char *ptr;
-	unsigned int i = 0;
+	char *str;
+	unsigned int i;
 
-	if (size == 0)
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	ptr = malloc(sizeof(char) * size);
-	if (ptr == NULL)
-		return (NULL);
-	while (i < size)
-	{
-		ptr[i] = c;
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	return (str);
 }
